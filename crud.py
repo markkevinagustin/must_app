aa7408790fb69ec1a6f13739b19eb504e3b315a6
fearless <ghost@fearlesss-Mac-mini.local>
2022-01-22 18:58:33 +0800

    First iteration

--- crud.py
+++ crud.py
@@ -11,6 +11,10 @@ def get_user(db: Session, data_id: int):
     return db.query(models.User).filter(models.User.data_id == data_id).first()
 
 
+def get_scheds(db: Session, data_id: int):
+    return db.query(models.Sched).filter(models.Sched.owner_id == data_id).all()
+
+
 def create_user(db: Session, data_id: str, name: str):
     db_user = models.User(data_id=data_id, name=name)
     db.add(db_user)
@@ -19,10 +23,8 @@ def create_user(db: Session, data_id: str, name: str):
     return db_user
 
 
-def create_user_sched(db: Session, busy_start: str,
-                      busy_end: str, data_id: str):
-    db_sched = models.Sched(busy_start=busy_start,
-                            busy_end=busy_end, owner_id=data_id)
+def create_user_sched(db: Session, busy_start: str, busy_end: str, data_id: str):
+    db_sched = models.Sched(busy_start=busy_start, busy_end=busy_end, owner_id=data_id)
     db.add(db_sched)
     db.commit()
     db.refresh(db_sched)
---
+++ utils.py
@@ -0,0 +1,161 @@
+from datetime import datetime, timedelta
+import dateutil.parser
+import pytz
+
+FMT = "%m/%d/%Y %I:%M:%S %p"
+RANGE = 30
+
+
+def validate(data, timezone, office_hours, meeting_length):
+    # validations for  meetings that start every whole and half hour, e.g. 08.00, 08.30, 09.00, etc.
+
+    earliest = data[0]
+    latest = data[1]
+
+    office_start  = earliest.replace(hour=int(office_hours[0])).replace(minute=0).replace(second=0)
+    office_end = latest.replace(hour=int(office_hours[1])).replace(minute=0).replace(second=0)
+
+    inside_left_bound = office_start.time()  <=  earliest.time()
+    inside_right_bound = office_end.time() >= latest.time()
+
+    earliest_plus_meeting_length = earliest + timedelta(minutes=meeting_length)
+    earliest_with_allowance = earliest_plus_meeting_length.time() <= office_end.time()
+
+    latest_plus_meeting_length = latest + timedelta(minutes=meeting_length)
+    latest_with_allowance = latest_plus_meeting_length.time() <= office_end.time()
+
+    earliest_minute = earliest.minute
+    latest_minute = latest.minute
+
+    if not earliest.time() < latest.time():
+        return (False, ["earliest should be less than latest"])
+
+    if not inside_left_bound:
+        return (False, ["earliest should be inside office_hours"])
+    if not inside_right_bound:
+        return (False, ["latest should be inside office_hours"])
+
+    if not earliest_with_allowance:
+        return (False, ["earliest should be {} minutes earlier to accomodate the meeting length".format(meeting_length)])
+    if not latest_with_allowance:
+        return (False, ["latest should be {} minutes earlier to accomodate the meeting length".format(meeting_length)])
+
+    if earliest_minute != 0 and earliest_minute != 30:
+        return (False, ["earliest should be in an increment of 30 mins"])
+    if latest_minute != 0 and latest_minute != 30:
+        return (False, ["latest should be in an increment of 30 mins"])
+
+    return (True, None)
+
+
+def daterange_requested(start, end, delta, office_hours, meeting_length):
+    current = start
+    latest_plus_meeting_length = end + timedelta(minutes=meeting_length)
+    end_plus_meeting_length = end + timedelta(minutes=meeting_length)
+    while current <= end_plus_meeting_length:
+        yield current
+        if current.time() == latest_plus_meeting_length.time():
+            current = current.replace(hour=start.hour).replace(minute=start.minute).replace(second=0) + timedelta(days=1)
+        else:
+            current += delta
+
+def daterange_daily(start, end, delta, office_hours):
+    current = start.replace(hour=int(office_hours[0])).replace(minute=0).replace(second=0)
+    current_end = end.replace(hour=int(office_hours[1])).replace(minute=0).replace(second=0)
+    while current <= current_end:
+        yield current
+        if current.time() == current_end.time():
+            current = current.replace(hour=int(office_hours[0])).replace(minute=0).replace(second=0) + timedelta(days=1)
+        else:
+            current += delta
+
+def busy_days_db(data, earliest_latest, timezone):
+    busy_start = datetime.strptime(str(data.busy_start), FMT).astimezone(pytz.UTC)
+    earliest = earliest_latest[0]
+    latest = earliest_latest[1]
+    if earliest.date() == busy_start.date() or latest.date() == busy_start.date():
+        data.busy_start = utc_to_client(data.busy_start, timezone).strftime(FMT)
+        data.busy_end = utc_to_client(data.busy_end, timezone).strftime(FMT)
+        return data
+
+def build_unavailable_scheds(schedules_db, office_hours, earliest_latest_datetime, timezone):
+    unavailable_scheds = [busy_days_db(sched, earliest_latest_datetime, timezone) for sched in schedules_db[0]]
+    unavailable_scheds_clean = [clean for clean in unavailable_scheds if clean]
+    unavailable_scheds_list_unflattened = []
+    for unavailable_sched in unavailable_scheds_clean:
+        busy_start = datetime.strptime(str(unavailable_sched.busy_start), FMT).astimezone(pytz.UTC)
+        busy_end = datetime.strptime(str(unavailable_sched.busy_end), FMT).astimezone(pytz.UTC)
+        unavailable_scheds_list_unflattened.append([
+            utc_to_client_str(dt, timezone)
+            for dt in datetime_range(
+                busy_start,
+                busy_end,
+                timedelta(minutes=RANGE),
+                office_hours
+            )])
+
+    return [item for sublist in unavailable_scheds_list_unflattened for item in sublist]
+ 
+
+def build_daily_scheds(earliest_latest_datetime, office_hours):
+    daily_start = earliest_latest_datetime[0]
+    daily_end = earliest_latest_datetime[1]
+    return [
+        dt.strftime(FMT)
+        for dt in daterange_daily(
+            daily_start,
+            daily_end,
+            timedelta(minutes=RANGE),
+            office_hours
+        )
+    ]
+
+def build_requested_meeting_scheds(earliest_latest_datetime, office_hours, meeting_length):
+    requested_start = earliest_latest_datetime[0]
+    requested_end = earliest_latest_datetime[1]
+    return [
+        dt.strftime(FMT)
+        for dt in daterange_requested(
+            requested_start,
+            requested_end,
+            timedelta(minutes=RANGE),
+            office_hours,
+            meeting_length
+        )
+    ]
+
+def datetime_range(start, end, delta, office_hours):
+    current = start
+    while current <= end:
+        yield current
+        current += delta
+
+def convert_to_datetime(data, timezone):
+    data_datetime = datetime.strptime(str(data), FMT)
+    return data_datetime.astimezone(pytz.timezone(timezone))
+
+
+def is_time(date):
+    try:
+        return datetime.strptime(str(date), FMT)
+    except ValueError:
+        return False
+        return False
+
+
+def client_to_utc(data):
+    data_datetime = datetime.strptime(str(data), FMT)
+    return data_datetime.astimezone(pytz.UTC)
+
+
+def utc_to_client(data, timezone):
+    utc_timezone = pytz.timezone("UTC")
+    data_datetime = dateutil.parser.parse(data)
+    parse_data_utc = utc_timezone.localize(data_datetime)
+    return parse_data_utc.astimezone(pytz.timezone(timezone))
+
+
+def utc_to_client_str(data, timezone):
+    data_str = data.strftime(FMT)
+    output = utc_to_client(data_str, timezone)
+    return datetime.strftime(output, FMT)
